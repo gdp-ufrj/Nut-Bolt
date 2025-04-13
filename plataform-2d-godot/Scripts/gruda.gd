e7f83b767545eb2892f540bde93d179738731127
Lucas Terra <lucas.terra8587@gmail.com>
2025-04-13 15:26:59 -0300

    gruda e pula adicionados

--- plataform-2d-godot/Scripts/gruda.gd
+++ plataform-2d-godot/Scripts/gruda.gd
@@ -3,23 +3,30 @@ extends CharacterBody2D
 #const SPEED = 300.0
 #const JUMP_VELOCITY = -400
 
-@export var SPEED: int = 300
-@export var JUMP_VELOCITY: int = -400
-@export var DISTANCE: int = 200
+@export var SPEED: int = 150
 #@export var other_character: Node2D
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var game_over = false  # Variável para impedir múltiplos prints
 
 func _physics_process(delta: float) -> void:
+	var direction = Input.get_axis("ui_left_WASD", "ui_right_WASD")
+	
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		
-	if Input.is_action_just_pressed("ui_up_WASD") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		
-		
-	var direction = Input.get_axis("ui_left_WASD", "ui_right_WASD")
+	
+	if is_on_wall() and Input.is_action_pressed("ui_right_WASD") and Input.is_action_pressed("ui_up_WASD"):
+		velocity.x = direction * SPEED
+		velocity.y = -gravity * delta * 5
+	
+	if is_on_wall() and Input.is_action_pressed("ui_left_WASD") and Input.is_action_pressed("ui_up_WASD"):
+		velocity.x = direction * SPEED
+		velocity.y = -gravity * delta * 5
+	
+	if is_on_ceiling() and Input.is_action_pressed("ui_up_WASD"):
+		velocity.x = 0
+		velocity.y = 0
+	
 	if direction:
 		velocity.x = direction * SPEED 
 	else:
--- plataform-2d-godot/Scripts/pula.gd
+++ plataform-2d-godot/Scripts/pula.gd
@@ -17,8 +17,7 @@ func _physics_process(delta: float) -> void:
 		
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		
-		
+	
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED 
