991ea744e55b953064c4b384c179c8216b566482
NicolasFlor <mickflor9@gmail.com>
2025-04-13 22:19:28 -0300

    Conexão entre players
    P1 e P2 agora ficam paralisados após ficarem 4 segundos longe.

--- plataform-2d-godot/Scripts/pula.gd
+++ plataform-2d-godot/Scripts/pula.gd
@@ -1,15 +1,27 @@
 extends CharacterBody2D
 
+
+#Cemitério de Código
 #const SPEED = 300.0
 #const JUMP_VELOCITY = -400
+#signal sem_sinal
+#var game_over = false  # Variável para impedir múltiplos prints
+#funcao que determina a distancia dos players
+	#if other_character:
+		##calcula a distancia do player 1 ao 2
+		#var distance = global_position.distance_to(other_character.global_position)
+		#if distance > DISTANCE and not game_over:
+			#game_over = true
+			#print("YOU RE DIE")
 
 @export var SPEED: int = 300
 @export var JUMP_VELOCITY: int = -400
-@export var DISTANCE: int = 200
-@export var other_character: Node2D
+@export var MAX_DISTANCE: int = 100
+@export var other_character: Node2D 
+@export var conectado: bool = true
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var game_over = false  # Variável para impedir múltiplos prints
+
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -24,14 +36,29 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x,0,SPEED)
 	
-	move_and_slide()
-		
-	#funcao que determina a distancia dos players
-	if other_character:
-		#calcula a distancia do player 1 ao 2
-		var distance = global_position.distance_to(other_character.global_position)
-		if distance > DISTANCE and not game_over:
-			game_over = true
-			print("YOU RE DIE")
-			
+	move_and_slide() 
+	is_conectado([other_character]) #aqui entram novos conectores
+
+ 
+func is_conectado(conectores: Array):
+	##Checa se existe conexão ativa e age de acordo
+	var distancia
+	for objeto in conectores:
+		if objeto != null:
+			distancia =  self.global_position.distance_to(objeto.global_position)
+			if distancia > MAX_DISTANCE:
+				if $Timer_sem_sinal.is_stopped(): #se o timer não já estiver ativo
+					$Timer_sem_sinal.start()
+			else:
+				$Timer_sem_sinal.stop()
+
+
+	
+func _on_timer_sem_sinal_timeout() -> void:
+	self.SPEED = 0
+	self.JUMP_VELOCITY = 0
+	other_character.SPEED = 0
+	conectado = false
+	
+	
 	
