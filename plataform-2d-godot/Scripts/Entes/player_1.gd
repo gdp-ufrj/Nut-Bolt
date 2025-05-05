9e187839bee0a245030e3e8981bfc15cc37be097
NicolasFlor <mickflor9@gmail.com>
2025-05-04 22:44:17 -0300

    conexão e camadas de colisão
    Ajustes no código da conexão e implementação de camadas de colisão para que os personagens não colidam entre si.

--- plataform-2d-godot/Scripts/Entes/player_1.gd
+++ plataform-2d-godot/Scripts/Entes/player_1.gd
@@ -2,18 +2,15 @@ extends CharacterBody2D
 
 @export var SPEED: int = 300
 @export var JUMP_VELOCITY: int = -400
-@export var MAX_DISTANCE: int = 250
+#@export var MAX_DISTANCE: int = 250
 @onready var timer = $Timer_conexao
+@onready var zona_conexao = $zona_conexao
 
 var estado_original: Array = [SPEED, JUMP_VELOCITY]
 var pode_ativar_botao = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
-func _ready() -> void:
-	pass
-
-
+#region Physics process
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -31,10 +28,10 @@ func _physics_process(delta: float) -> void:
 	
 	if pode_ativar_botao and Input.is_action_just_pressed("interagir"):
 		$"../plataforma/AnimationPlayer".play("move")
-	
-	
+		
+#endregion
 
-
+#region Tutorial 2 
 func _on_botao_body_entered(body: Node2D) -> void:
 	if body == self:
 		$"../Botao/Label".visible = true
@@ -56,8 +53,10 @@ func _on_area_para_evitar_bug_body_entered(body: Node2D) -> void:
 func _on_area_para_evitar_bug_body_exited(body: Node2D) -> void:
 	if body == self:
 		$"../plataforma/AnimationPlayer".play("move")
-
+		
+#endregion
 	
+#region Conexão
 func conectar()->void:
 	SPEED = estado_original[0]
 	JUMP_VELOCITY = estado_original[1]
@@ -70,9 +69,13 @@ func desconectar()-> void:
 func _on_timer_conexao_timeout() -> void:
 	desconectar()
 
-func _on_player_1_area_entered(area: Area2D) -> void:
-	conectar()
+func _on_zona_conexao_area_entered(area: Area2D) -> void:
+	if area.name == "zona_conexao":
+		conectar()
 
-func _on_player_1_area_exited(area: Area2D) -> void:
-	if timer.is_stopped(): 
-		timer.start()
+func _on_zona_conexao_area_exited(area: Area2D) -> void:
+	if area.name == "zona_conexao":
+		if timer.is_stopped(): 
+			timer.start()
+
+#endregion
--- plataform-2d-godot/Scripts/Entes/player_2.gd
+++ plataform-2d-godot/Scripts/Entes/player_2.gd
@@ -2,11 +2,14 @@ extends CharacterBody2D
 
 @export var SPEED: int = 150
 @onready var timer = $Timer_conexao
+@onready var zona_conexao = $zona_conexao
+
 var estado_original = SPEED
 var pode_ativar_botao = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
+#region physics process
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_axis("ui_left_WASD", "ui_right_WASD")
 	
@@ -34,8 +37,9 @@ func _physics_process(delta: float) -> void:
 	
 	if pode_ativar_botao and Input.is_action_just_pressed("interagir"):
 		$"../plataforma/AnimationPlayer".play("move")
-	
+#endregion
 
+#region Tutorial 2
 # Funcao para quando o personagem entra na area do botao
 func _on_botao_body_entered(body: Node2D) -> void:
 	if body == self:
@@ -59,7 +63,9 @@ func _on_area_para_evitar_bug_body_entered(body: Node2D) -> void:
 func _on_area_para_evitar_bug_body_exited(body: Node2D) -> void:
 	if body == self:
 		$"../plataforma/AnimationPlayer".play("move")
+#endregion
 
+#region conexao
 func conectar()->void:
 	SPEED = estado_original
 	timer.stop()
@@ -70,10 +76,13 @@ func desconectar()-> void:
 func _on_timer_conexao_timeout() -> void:
 	desconectar()
 
-func _on_player_2_area_entered(area: Area2D) -> void:
-	conectar()
+func _on_zona_conexao_area_entered(area: Area2D) -> void:
+	if area.name == "zona_conexao":
+		conectar()
 
+func _on_zona_conexao_area_exited(area: Area2D) -> void:
+	if area.name == "zona_conexao":
+		if timer.is_stopped():
+			timer.start()
 
-func _on_player_2_area_exited(area: Area2D) -> void:
-	if timer.is_stopped():
-		timer.start()
+#endregion
