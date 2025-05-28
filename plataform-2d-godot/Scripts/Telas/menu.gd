683a49811842c3d8af23e613e58da8f2d9a82277
Thurjo <joaoarthur487@gmail.com>
2025-05-28 19:37:29 -0300

    Esqueleto_OptionsMenu

--- plataform-2d-godot/Scripts/Telas/menu.gd
+++ plataform-2d-godot/Scripts/Telas/menu.gd
@@ -1,4 +1,10 @@
-extends Node2D
+extends Control
+
+@onready var options_menu = $Opcoes_Menu as OptionsMenu
+@onready var canvaslayer = $CanvasLayer as CanvasLayer
+
+func _ready():
+	handle_connecting_signals()
 
 #botao de start carregando as cenas do game controller
 func _on_start_pressed() -> void:
@@ -14,4 +20,14 @@ func _on_creditos_pressed() -> void:
 
 #botão de Opções ainda ainda não completado 
 func _on_opções_pressed() -> void:
-	pass # Replace with function body.
+	canvaslayer.visible = false
+	options_menu.set_process(true)
+	options_menu.visible = true
+	
+func _on_exit_options_menu() -> void:
+	canvaslayer.visible = true
+	options_menu.visible = false
+
+func handle_connecting_signals() -> void:
+	options_menu.exit_options_menu.connect(_on_exit_options_menu)
+	
