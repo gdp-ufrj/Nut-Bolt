8718b9bc033333c14e2dab7a57206a4d0623c573
Lucas Terra <lucas.terra8587@gmail.com>
2025-07-12 03:05:33 -0300

    bastante progresso

--- plataform-2d-godot/Scripts/Entes/player_2.gd
+++ plataform-2d-godot/Scripts/Entes/player_2.gd
@@ -4,46 +4,44 @@ extends CharacterBody2D
 @onready var conexao = $Conexao_player_2
 
 @export var SPEED: int = 90
-@onready var raycast_chao = $RayCast2D_Centro
-@onready var raycast_direita = $RayCast2D_direita
-@onready var raycast_esquerda = $RayCast2D_esquerda
-@onready var raycast_esquerda_meio = $RayCast2D_esquerda_meio
-@onready var raycast_direita_meio = $RayCast2D_direita_meio
+@export var gravity_speed = 400
 
-var colidiu_com_limites = false
-var pode_grudar = true
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var desacoplou = false
+var unit_direction: Vector2
+var gravity = Vector2.ZERO
 var esta_desativado = false
-var som_desligar_ja_tocado = false
-var grudando: bool = false
+
+var state = States.CAINDO
+var prev_state = null
+var state_timeout = false
+
+enum States {
+	CHAO,
+	PAREDE_ESQUERDA,
+	PAREDE_DIREITA,
+	TETO,
+	CAINDO,
+	DESACOPLOU
+}
 
 func _ready() -> void:
+	#velocity = Vector2.ZERO
 	$zona_conexao_2.collision_layer = 3
 	$zona_conexao_2.collision_mask = 3
 	self.add_to_group("Players")
 	conexao.conectar()
-	
 
 #region physics process
 func _physics_process(delta: float) -> void:
-	
 	var direction = Input.get_axis("ui_left_WASD", "ui_right_WASD")
-	
-	#Grudar ativar e desativar
-	if Input.is_action_just_pressed("toggle_grudar"):
-		grudando = not grudando
-		if grudando:
-			print ("Modo GRUDAR Ativado")
-		else:
-			print ("Modo GRUDAR Desativado")
-			# Restaurar gravidade e rotação quando desgrudar
-			gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-			$"Animação".rotation = 0
-			$"Animação".flip_v = false
-	
+	process_input()
+	process_gravity()
+	velocity += gravity * delta
+	move_and_slide()
+	update_state()
+	print(state)
+	voltou_ao_chao()
 	# SOM DE ANDAR
-	if Input.is_action_pressed("ui_left_WASD") or Input.is_action_pressed("ui_right_WASD") or Input.is_action_pressed("ui_up_WASD") and is_on_floor() and not esta_desativado:
+	if Input.is_action_pressed("ui_left_WASD") or Input.is_action_pressed("ui_right_WASD") and is_on_floor() and not esta_desativado:
 		if not $audio_andar.is_playing():
 			$audio_andar.play()
 	else:
@@ -52,96 +50,134 @@ func _physics_process(delta: float) -> void:
 	if esta_desativado:
 		$audio_andar.stop()
 	
-	# SOM DE POUSO
-	if colidiu_com_limites and is_on_floor():
-		pode_grudar = true
-		if desacoplou:
-			$audio_pouso.play()
-			desacoplou = false
-
-	grudar(delta,direction)
-	
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x,0,SPEED)
-		
 	animation.setAnimation(direction, esta_desativado)
-	move_and_slide()
 #endregion
 
-#region movimentação
-# FUNCAO PARA IDENTIFICAR SE ENCOSTA EM ALGUMA BARREIRA (MOVIMENTACAO)
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group('limites'):
-		colidiu_com_limites = true
+func process_input():
+	if state == States.CHAO or state == States.TETO or state == States.CAINDO:
+		if state != States.CAINDO:
+			velocity.x = 0
+	if Input.is_action_pressed("ui_right_WASD") and state == States.CHAO:
+		velocity.x += SPEED
+		if ray_right():
+			velocity.y += SPEED
+			enter_state(States.PAREDE_DIREITA)
+	if Input.is_action_pressed("ui_left_WASD") and state == States.CHAO:
+		velocity.x -= SPEED
+		if ray_left():
+			velocity.y += SPEED
+			enter_state(States.PAREDE_ESQUERDA)
+	if Input.is_action_pressed("ui_down_WASD") and state == States.TETO:
+		if ray_right():
+			enter_state(States.PAREDE_DIREITA)
+		elif ray_left():
+			enter_state(States.PAREDE_ESQUERDA)
+		else:
+			enter_state(States.CAINDO)
+	
+	if state == States.PAREDE_ESQUERDA:
+		velocity.y = 0
+		if Input.is_action_pressed("ui_right_WASD"):
+			velocity.y += SPEED
+		if Input.is_action_pressed("ui_left_WASD"):
+			velocity.y -= SPEED
+	
+	if state == States.PAREDE_DIREITA:
+		velocity.y = 0
+		if Input.is_action_pressed("ui_left_WASD"):
+			velocity.y += SPEED
+		if Input.is_action_pressed("ui_right_WASD"):
+			velocity.y -= SPEED
+	
+	if state == States.PAREDE_ESQUERDA or state == States.PAREDE_DIREITA:
+		if ray_down():
+			if Input.is_action_pressed("ui_right_WASD") and !ray_right():
+				enter_state(States.CHAO)
+			if Input.is_action_pressed("ui_left_WASD") and !ray_left():
+				enter_state(States.CHAO)
+		elif ray_up():
+			if Input.is_action_pressed("ui_right_WASD") and !ray_right():
+				enter_state(States.TETO)
+			if Input.is_action_pressed("ui_left_WASD") and !ray_left():
+				enter_state(States.TETO)
+		else:
+			if Input.is_action_pressed("ui_right_WASD") and !ray_right():
+				enter_state(States.CAINDO)
+			if Input.is_action_pressed("ui_left_WASD") and !ray_left():
+				enter_state(States.CAINDO)
+	
+	# Leitura de desacoplamento -> TECLA 'S'
+	if state != States.DESACOPLOU:
+		if Input.is_action_pressed("ui_down_WASD"):
+			enter_state(States.DESACOPLOU)
+	
+	if state == States.CAINDO and prev_state == States.PAREDE_ESQUERDA or prev_state == States.PAREDE_DIREITA:
+		if Input.is_action_pressed("ui_right_WASD"):
+			velocity.x += 0.5
+			velocity.y -= 0.5
+			if is_on_floor():
+				enter_state(States.CHAO)
+		if Input.is_action_pressed("ui_left_WASD"):
+			velocity.x -= 0.5
+			velocity.y -= 0.5
+			if is_on_floor():
+				enter_state(States.CHAO)
+	
+	if state == States.CAINDO and prev_state == States.CHAO and !is_on_floor():
+		if Input.is_action_pressed("ui_right_WASD"):
+			velocity.x = 0
+			velocity.y -= 0.5
+			if is_on_wall():
+				enter_state(States.PAREDE_ESQUERDA)
 
-# FUNCAO PARA IDENTIFICAR SE DESENCOSTA DE ALGUMA BARREIRA (MOVIMENTACAO)
-# SOM DE DESACOPLAR
-func _on_area_2d_body_exited(body: Node2D) -> void:
-	if body.is_in_group('limites'):
-		colidiu_com_limites = false
-		pode_grudar = false
-		$audio_desacoplar.play()
-		desacoplou = true
-		gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+func process_gravity():
+	if state == States.PAREDE_DIREITA:
+		gravity = Vector2(gravity_speed, 0)
+	elif state == States.PAREDE_ESQUERDA:
+		gravity = Vector2(-gravity_speed, 0)
+	elif state == States.TETO:
+		gravity = Vector2(0, -gravity_speed)
+	else:
+		gravity = Vector2(0, gravity_speed)
 
-func grudar(delta, direction)->void:
-	if not is_on_floor():
-		velocity.y += gravity * delta
-		
-	if not grudando:
-		return
-	#VERIFICA APOIO REAL MUdanca
-	var em_chao = raycast_chao.is_colliding()
-	var em_direita = raycast_direita.is_colliding()
-	var em_esquerda = raycast_esquerda.is_colliding()
-	var em_esquerda_meio = raycast_esquerda_meio.is_colliding()
-	var em_direita_meio = raycast_direita_meio.is_colliding()
+func update_state():
+	if !ray_left() and !ray_right() and !ray_down() and !ray_up() and !(state == States.CAINDO):
+		enter_state(States.CAINDO)
+	
+	if state == States.CAINDO:
+		if ray_down():
+			enter_state(States.CHAO)
+		if ray_up():
+			enter_state(States.TETO)
+		if ray_left():
+			enter_state(States.PAREDE_ESQUERDA)
+		if ray_right():
+			enter_state(States.PAREDE_DIREITA)
 
-	#CONDICAO PARA DESLIGAR O MODO GRUDAR AUTOMATICAMENTE, NAO CONSEGUI FAZER
-	#if not em_chao and not em_direita and not em_esquerda and not em_esquerda_meio and not em_direita_meio:
-		#grudando = false
-		#print("Desgrudado automaticamente por falta de apoio")
-		#return
-		
-	if is_on_ceiling():
-		$"Animação".rotation = 0
-		$"Animação".flip_v = true
-		
-	if is_on_floor():
-		$"Animação".rotation = 0
-		$"Animação".flip_v = false
-		
-	if is_on_wall() and Input.is_action_pressed("ui_right_WASD") and pode_grudar:
-		velocity.x = direction * SPEED
-		velocity.y = gravity * delta * 4
-		$"Animação".rotation_degrees = -90
-		if(Input.is_action_pressed("ui_up_WASD")):
-			velocity.x = direction * SPEED
-			velocity.y = -gravity * delta * 5
-	
-	if is_on_wall() and Input.is_action_pressed("ui_left_WASD") and pode_grudar:
-		velocity.x = direction * SPEED
-		velocity.y = gravity * delta * 4
-		$"Animação".rotation_degrees = 90
-		#animation.flip_h = false
-		if(Input.is_action_pressed("ui_up_WASD")):
-			velocity.x = direction * SPEED
-			velocity.y = -gravity * delta * 5
-	
-	if is_on_ceiling() and (Input.is_action_pressed("ui_left_WASD") or Input.is_action_pressed("ui_right_WASD")):
-		if(colidiu_com_limites):
-			gravity = -50
-			velocity.y = -velocity.y
-	
-	if is_on_ceiling() and Input.is_action_pressed("ui_down_WASD"):
-		if gravity == -50:
-			gravity = 980
-			velocity.y = -velocity.y
-	
-	if is_on_wall() and gravity == -50:
-		if Input.is_action_pressed("ui_left_WASD") or Input.is_action_pressed("ui_right_WASD"):
-			gravity = 980
-			velocity.y = -velocity.y
-#endregion
+func enter_state(new_state):
+	if new_state != prev_state or !state_timeout:
+		prev_state = state
+		state = new_state
+		$StateChangeTimer.start()
+		state_timeout = true
+
+func _on_state_change_timer_timeout() -> void:
+	state_timeout = false
+
+func voltou_ao_chao():
+	if state == States.DESACOPLOU:
+		if is_on_floor():
+			enter_state(States.CHAO)
+
+# Funcoes auxiliares para detectar colisao nos raycasts
+func ray_up():
+	return $Raycasts/RayUp.is_colliding()
+
+func ray_down():
+	return $Raycasts/RayDown.is_colliding()
+
+func ray_right():
+	return $Raycasts/RayRight.is_colliding()
+
+func ray_left():
+	return $Raycasts/RayLeft.is_colliding()
