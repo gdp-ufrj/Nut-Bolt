3fcef9cf4c63aef697207ee8152ff416fc218f66
NicolasFlor <mickflor9@gmail.com>
2025-07-23 11:46:11 -0300

    Revert "Parede retrátil fase 2"
    This reverts commit debfe65665ad1a0e70d8aff216821b360f905fdc.

--- plataform-2d-godot/Scripts/Animações/animation_1.gd
+++
@@ -1,7 +0,0 @@
-extends AnimationPlayer
-
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("restart"):
-		#self.stop()
-		#self.active = false
-		pass
--- plataform-2d-godot/Scripts/Niveis/fase_1.gd
+++ plataform-2d-godot/Scripts/Niveis/fase_1.gd
@@ -6,7 +6,6 @@ extends Node2D
 @onready var botao4: Area2D = $Botoes/Botao4
 @onready var botao5: Area2D = $Botoes/Botao5
 @onready var botoes: Array = [botao1,botao2,botao3,botao4,botao5]
-
 @onready var parede1: AnimationPlayer = $Paredes_Retrateis/PR1/AnimationPlayer
 @onready var parede2: AnimationPlayer = $Paredes_Retrateis/PR2/AnimationPlayer
 @onready var parede3: AnimationPlayer = $Paredes_Retrateis/PR3/AnimationPlayer
@@ -19,7 +18,7 @@ func _process(delta: float) -> void:
 	var i: int = 0
 	for botao in botoes:
 		if botao.pode_ativar and Input.is_action_just_pressed("interagir"):
-			paredes[i].play("Abrir")
+			paredes[i].play("Retrair")
 			botao.is_untouched = false
 			botao.emit_signal("ativar_oneshot")
 		i+=1
