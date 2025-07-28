5730f8bf19f85a7a2cb69f6972e349d04b67d4b1
jojo <joanaalexandra.p@gmail.com>
2025-07-28 01:20:29 -0300

    Revert "Dialogos dos Coletaveis (incompleto)"
    This reverts commit 1164b0a90adab74b0795424dbf8cb4fc73820abf.

--- plataform-2d-godot/Scripts/Telas/diary_popup.gd
+++ plataform-2d-godot/Scripts/Telas/diary_popup.gd
@@ -1,7 +1,5 @@
 extends CanvasLayer
 
-signal diario_fechado
-
 @onready var text_box: RichTextLabel = $Panel/ScrollContainer/MarginContainer/VBoxContainer/DiaryText
 @onready var button: Button = $Panel/ScrollContainer/MarginContainer/VBoxContainer/Fechar
 
@@ -23,7 +21,6 @@ func _on_close_pressed():
 	get_tree().paused = false
 	hide()
 	is_showing = false
-	emit_signal("diario_fechado")
 	
 func reset():
 	hide()
